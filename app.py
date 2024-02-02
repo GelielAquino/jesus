c08510e30f050fff0910518aaf5119cd3a015951
GelielAquino <149223223+GelielAquino@users.noreply.github.com>
2024-02-02 16:53:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 import streamlit as st
+from datetime import datetime
 import time
 
-st.title("Jesus")
-start_button = st.button("Iniciar/Reiniciar Atualização")
-
-if start_button:
-    counter = 0
-    while counter < 5:
-        st.write("Jesus")
-        time.sleep(1)
-        counter += 1
-        if counter == 10:
-            st.empty()  # Limpar a saída após as 10 repetições
+# Função para obter o horário atual
+def obter_horario_atual():
+    return datetime.now().strftime('%H:%M:%S')
 
 
 
